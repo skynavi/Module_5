729ac6c119e12fe508ecea7895ff6673808ff485
skynavi <skynavi@mail.ru>
2024-08-06 22:23:53 +0300

    0608

---
+++ Module_5_4.py
@@ -0,0 +1,94 @@
+class House:
+    houses_history = []
+    def __new__(cls, *args, **kwargs):
+        cls.houses_history.append(args[0])
+        return object.__new__(cls)
+
+    def __init__(self, name, number_of_floors):
+        self.name = name
+        self.number_of_floors = number_of_floors
+
+    def __str__(self):
+        return f' Название: {self.name}, кол-во этажей: {self.number_of_floors}'
+
+    def __add__(self, value):
+        if isinstance(value, House):
+            self.number_of_floors + value
+        elif isinstance(value, int):
+            self.number_of_floors = self.number_of_floors + value
+        return self
+
+    def __iadd__(self, value):
+        if isinstance(value, House):
+            self.number_of_floors += value.number_of_floors
+        elif isinstance(value, int):
+            self.number_of_floors += value
+        return self
+
+    def __radd__(self, value):
+        if isinstance(value, House):
+            self.number_of_floors = value
+        elif isinstance(value, int):
+            self.number_of_floors = value + self.number_of_floors
+        return self
+
+    def __eq__(self, other):
+        return self.number_of_floors == other.number_of_floors
+
+    def __lt__(self, other):
+        return self.number_of_floors < other.number_of_floors
+
+    def __le__(self, other):
+        return self.number_of_floors <= other.number_of_floors
+
+    def __gt__(self, other):
+        return self.number_of_floors > other.number_of_floors
+
+    def __ge__(self, other):
+        return self.number_of_floors >= other.number_of_floors
+
+    def __ne__(self, other):
+        return self.number_of_floors != other.number_of_floors
+
+    def __del__(self):
+        print(f' {self.name} снесён, но он останется в истории')
+
+
+# h1 = House('ЖК Эльбрус', 10)
+# h2 = House('ЖК Акация', 20)
+#
+# print(h1)
+# print(h2)
+#
+# print(h1 == h2)  # __eq__
+#
+# h1 = h1 + 10  # __add__
+# print(h1)
+# print(h1 == h2)
+#
+# h1 += 10  # __iadd__
+# print(h1)
+#
+# h2 = 10 + h2  # __radd__
+# print(h2)
+#
+# print(h1 > h2)  # __gt__
+# print(h1 >= h2)  # __ge__
+# print(h1 < h2)  # __lt__
+# print(h1 <= h2)  # __le__
+# print(h1 != h2)  # __ne__
+
+h1 = House('ЖК Эльбрус', 10)
+print(House.houses_history)
+h2 = House('ЖК Акация', 20)
+print(House.houses_history)
+h3 = House('ЖК Матрёшки', 20)
+print(House.houses_history)
+
+# Удаление объектов
+del h2
+del h3
+
+print(House.houses_history)
+
+
