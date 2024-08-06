2364fab921324ac9d8db43372b9c146be51c05cb
skynavi <skynavi@mail.ru>
2024-08-07 00:15:07 +0300

    0608

---
+++ classpract1.py
@@ -0,0 +1,21 @@
+class Database:
+    def __init__(self):
+        self.data = {}
+    def add_user(self, username, password):
+        self.data[username] = password
+
+class User:
+    """
+            ввод пользователя и пароля
+    """
+    def __init__(self, username, password, password_confirm):
+        self.username = username
+        if password == password_confirm:
+            self.password = password
+
+if __name__ == '__main__':
+    database = Database()
+    user = User(input('ВВедите логин:'), input('Введите пароль:'), input('Повторите пароль:'))
+    database.add_user(user.username, user.password)
+
+
